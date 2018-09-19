386f29df97905edbce7bc8f94323a56becdbfe02
Coral Sheldon-Hess <coral@sheldon-hess.org>
2018-09-19 14:39:19 -0400

    Formatting to make Markdown happy

--- runthestats.py
+++ runthestats.py
@@ -27,13 +27,13 @@ with open('jail_census_07_2017.csv', 'r') as census:
 				total_unknown_race_inmates += 1
 			total_inmates += 1
 
-
+# adding two spaces before the endline, to make Markdown happy
 with open('README.md', 'a') as output:
-	output.write('\n\t* Total inmates: %i\n' %total_inmates)
-	output.write('\t* Total Black inmates: %i, %.2f%%\n' %(total_black_inmates, total_black_inmates/total_inmates * 100))
-	output.write('\t* Total Asian inmates: %i, %.2f%%\n' %(total_asian_inmates, total_asian_inmates/total_inmates * 100))
-	output.write('\t* Total Hispanic inmates: %i, %.2f%%\n' %(total_hispanic_inmates, total_hispanic_inmates/total_inmates * 100))
-	output.write('\t* Total Indigenous inmates: %i, %.2f%%\n' %(total_indigenous_inmates, total_indigenous_inmates/total_inmates * 100))
-	output.write('\t* Total white inmates: %i, %.2f%%\n' %(total_white_inmates, total_white_inmates/total_inmates * 100))
-	output.write('\t* Total inmates of unknown race: %i, %.2f%%\n' %(total_unknown_race_inmates, total_unknown_race_inmates/total_inmates * 100))
+	output.write('\n* Total inmates: %i\n' %total_inmates)
+	output.write('* Total Black inmates: %i, %.2f%%  \n' %(total_black_inmates, total_black_inmates/total_inmates * 100))
+	output.write('* Total Asian inmates: %i, %.2f%%  \n' %(total_asian_inmates, total_asian_inmates/total_inmates * 100))
+	output.write('* Total Hispanic inmates: %i, %.2f%%  \n' %(total_hispanic_inmates, total_hispanic_inmates/total_inmates * 100))
+	output.write('* Total Indigenous inmates: %i, %.2f%%  \n' %(total_indigenous_inmates, total_indigenous_inmates/total_inmates * 100))
+	output.write('* Total white inmates: %i, %.2f%%  \n' %(total_white_inmates, total_white_inmates/total_inmates * 100))
+	output.write('* Total inmates of unknown race: %i, %.2f%%  \n' %(total_unknown_race_inmates, total_unknown_race_inmates/total_inmates * 100))
 
